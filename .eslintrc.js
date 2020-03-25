fae29384c007532e533a976c54a4779caee0f754
JanChodorowski <chodorowski.j@gmail.com>
2020-03-25 16:43:35 +0100

    Add eslint rules and plugins, husky pre commit hook, .nvmrc, docker-compose.yml, graphQL packages, tasks module

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,35 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'max-len': [
+      'error',
+      {
+        code: 120,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+        ignoreComments: true,
+      },
+    ],
+    quotes: 'off',
+    'no-multiple-empty-lines': ['warn', { max: 2 }],
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'semi',
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: false,
+        },
+      },
+    ],
+    '@typescript-eslint/quotes': [
+      'error',
+      'single',
+      { avoidEscape: true, allowTemplateLiterals: true },
+    ],
+    'import/default': [0],
   },
 };
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodosModule } from './todos/todos.module';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [GraphQLModule.forRoot({}), TodosModule],
   controllers: [AppController],
   providers: [AppService],
 })
